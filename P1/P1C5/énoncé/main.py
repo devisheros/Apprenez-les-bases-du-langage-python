805470a715fe72e95f25c3f23535057aedff512d
devisheros <blahdavid12@gmail.com>
2025-11-26 16:14:52 +0000

    #Résolution de l'exercice P1C5

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,15 @@
 # Écrivez votre code ici !
+Nom = "David"
+Age = 34
+Taille = 1.75
+Est_Estudiant = True
+
+print (f"* Nom: {Nom}")
+print (f"* Age: {Age}")
+print (f"* Taille: {Taille}")
+print (f"* Est étudiant: {Est_Estudiant}")
+
+print (f"* Type nom: {type(Nom)}")
+print (f"* Type age: {type(Age)}")
+print (f"* Type taille: {type(Taille)}")
+print (f"*Type est étudiant: {type(Est_Estudiant)}")
