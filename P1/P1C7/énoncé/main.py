1ff92565d8a4e44df26d6652c5af00719db0838a
devisheros <blahdavid12@gmail.com>
2025-11-26 18:55:57 +0000

    #Résolution de l'exercice P1C7

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,13 @@
 # Écrivez votre code ici !
+#1.creation du dictionnaire fruits
+fruits = {
+    "pomme":"rouge",
+    "banane":"jaune",
+    "orange":"orange",}
+fruits["kiwi"] = "vert"
+print(fruits)
+couleur_banane = fruits["banane"]
+print(couleur_banane)
+fruits["pomme"] = "vert"
+fruits.pop("banane")
+print(fruits.keys())
