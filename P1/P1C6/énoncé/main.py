600b67f2777c4b020c6af7ba614639479bbcb2b7
devisheros <blahdavid12@gmail.com>
2025-11-26 17:27:51 +0000

    #Résolution de l'exercice P1C6

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,10 @@
 # Écrivez votre code ici !
+fruits=["pomme","banane", "orange"]
+fruits.append("kiwi")
+print(fruits)
+fruits.remove("orange")
+print (fruits)
+fruits[1]="ananas"
+print (fruits)
+fruits.sort()
+print (fruits)
