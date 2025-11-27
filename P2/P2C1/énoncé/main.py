7d7b9d8ef94c5d615cb9e74cf3019a26e644a6bd
devisheros <blahdavid12@gmail.com>
2025-11-27 12:56:48 +0000

    #Résolution de l'exercice P1C7

--- P2/P2C1/énoncé/main.py
+++ P2/P2C1/énoncé/main.py
@@ -1,1 +1,41 @@
 # Ecrivez votre code ici !
+Nombre1 = input ("Entrer un nombre entier")
+Nombre2 = input ("Entrer un nombre entier")
+
+#is.numeric permet de vérifier que le caractère entré est un nombre
+
+if not Nombre1.isnumeric() or not Nombre2.isnumeric() :
+    print ("Erreur: les deux nombres doivent etre des nombres entiers")
+    raise SystemExit("Fin du programme")
+Nombre1 = int(Nombre1)
+Nombre2 = int(Nombre2)
+
+operation = input(f"Entrer un signe entre '+', '-','*' et '/'  pour effectuer votre operation")
+if operation not in ['+', '-', '*','/'] :
+    print("Erreur:vous avez pas entrer le bon signe")
+    raise SystemExit("Fin du programme")
+
+if operation == "+":
+    resultat = Nombre1 + Nombre2
+    
+elif operation == "-" :
+    resultat = Nombre1 - Nombre2
+
+elif operation == "*":
+    resultat = Nombre1 * Nombre2
+
+elif operation == "/":
+    if Nombre2 == 0 :
+        print("Erreur: impossible de diviser un nombre par Zero")
+        raise SystemExit("Fin du programme")
+    resultat = round (Nombre1 / Nombre2, 2)
+
+print (f"le resultat de l'operation est : {round(resultat, 2)}")
+    
+    
+
+
+
+
+
+
